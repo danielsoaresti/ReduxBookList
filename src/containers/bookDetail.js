c723c4ff3167a4e59e396877c88ae5373c820bc4
danielsoaresti <danielsoares.ti@gmail.com>
2016-06-08 10:48:28 -0400

    first commit

---
+++ src/containers/bookDetail.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+
+   render () {
+      return (
+         <div>{ this.props.book ? this.props.book.title : '' }</div>
+      );
+   }
+
+}
+
+
+function mapStateToProps(state) {
+  return {
+     book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
